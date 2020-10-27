3ce1ba9a3ee34db945cc837625493b76b669507c
Juan Andres Gonzalez Trejo <gordonfreenan@gmail.com>
2020-10-27 18:42:57 -0300

    Updates

---
+++ precache-manifest.62493c886f4101e458a75f8b56aeadf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619e0535d795583799ec6422ef28bdfc",
+    "url": "/reacsmalltest/index.html"
+  },
+  {
+    "revision": "be044779a00054d6d7a9",
+    "url": "/reacsmalltest/static/css/main.5d9eff6e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/reacsmalltest/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reacsmalltest/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be044779a00054d6d7a9",
+    "url": "/reacsmalltest/static/js/main.655e8820.chunk.js"
+  },
+  {
+    "revision": "3188f7d9b69f187b1ab1",
+    "url": "/reacsmalltest/static/js/runtime-main.793412b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacsmalltest/static/media/logo.5d5d9eef.svg"
+  }
+]);
